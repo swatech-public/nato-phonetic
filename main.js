2d286ed59af7e1c105349ebc7bef04f7f32515c6
Kelly Setzer <setzer@liquidchicken.org>
2022-01-29 17:55:52 -0600

    Rename project to nato-phonetic

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const Phonetic = require("./lib/phonetic.js");
+const Phonetic = require("./lib/nato-phonetic.js");
 
 const str = new Phonetic(process.argv[2]);
 console.log(str.toPhonetic());
